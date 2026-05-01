31883cfe971a0b13ac8ca166f5f6858709df7094
kavyashreekabilan007 <cb.sc.u4cse25526@cb.students.amrita.edu>
2026-05-01 21:31:35 +0530

    done some logical changes and styles restart button

--- guess.js
+++ guess.js
@@ -6,27 +6,40 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 function guessTheNumber() {
     let guessedNumber = Number(document.getElementById("guessedNumber").value);// Get the guessed number from the input field
     let clue;
-    score++;//****count the first attempt as well******
+
     document.getElementById("numberOfGuesses").innerHTML = score;
+
+    if (score >= 20) {
+        clue = "you are out of guesses, Try Again"
+        document.getElementById("clue").innerHTML = clue;
+        return;
+    }
+
+    score++; // Only increment if less than 20//****count the first attempt as well******
     guessedNumberList.push(guessedNumber);
     document.getElementById("guessedNumbers").innerHTML = guessedNumberList.join(" , ");
+
     if (guessedNumber === randomNumber) {
         clue = "Congratulations! You guessed the number!";
         document.getElementById("clue").innerHTML = clue;
         if (score < highscore) {
             highscore = score;
             document.getElementById("highscore").innerHTML = highscore;
+
         }
         return;//stopping user to guess after winning the game
     }
     else if (guessedNumber < randomNumber) {
         clue = "higher";
         document.getElementById("clue").innerHTML = clue;
+        document.getElementById("guessedNumber").value = "";
     }
     else {
         clue = "lower";
         document.getElementById("clue").innerHTML = clue;
+        document.getElementById("guessedNumber").value = "";
     }
+
 
 }
 function again() {
